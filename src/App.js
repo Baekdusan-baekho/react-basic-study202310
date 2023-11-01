34d34fc8cecb5837160ae0a78800d7d4264cedb6
lee1234 <whdtjs152413@gmail.com>
2023-11-01 11:11:21 +0900

    useState 기초

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Expenses from './components/Expenses';
+import Hello from './components/Hello';
 // 함수명은 파일명으로 지정하는 것이 정석
 const App = () => {
   // 지출 항목 객체 배열
@@ -23,9 +24,18 @@ const App = () => {
     },
   ];
 
+  console.log('App 실행~');
+
   return (
     <>
       <Expenses items={expenses} />
+      {/* <Hello>
+        <ul>
+          <li>사과</li>
+          <li>포도</li>
+          <li>복숭아</li>
+        </ul>
+      </Hello> */}
     </>
   );
 };
